3b9a4160ed4c6653c6292b0cc2e29c8350bb9ecd
Jeffrey Klassen <jeff.klassen@siginttech.com>
2016-03-18 15:42:34 +0100

    made S3_HOST param required

--- server/config/config.js
+++ server/config/config.js
@@ -9,19 +9,17 @@ config.options = { root: path.normalize(__dirname + '/../../') };
 config.s3 = function buildAWSS3() {
     var ep = null;
     var s3;
-    if (!process.env.REGION || !process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
+
+    if (!process.env.REGION || !process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY || !process.env.S3_HOST) {
         console.log("You must enter an AWS region, aws_access_key_id, and aws_secret_access_key");
-        console.log("Usage: REGION=us-east-1 AWS_ACCESS_KEY_ID=akid WS_SECRET_ACCESS_KEY=asak [HOST=hostname] npm start");
+        console.log("Usage: REGION=us-east-1 AWS_ACCESS_KEY_ID=akid AWS_SECRET_ACCESS_KEY=asak S3_HOST=hostname npm start");
         process.exit(1);
     }
     AWS.config.update({ region: process.env.REGION, accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY });
 
-    if (process.env.HOST) {
-        ep = new AWS.Endpoint(process.env.HOST || defaultAWSHost);
-        s3 = new AWS.S3({ endpoint: ep });
-    } else {
-        s3 = new AWS.S3();
-    }
+    ep = new AWS.Endpoint(process.env.S3_HOST);
+    s3 = new AWS.S3({ endpoint: ep });
+
     return s3;
 
 }
